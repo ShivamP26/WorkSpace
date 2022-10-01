339bbd24743f8c0af7729cf153275e3d69329e10
Shivam Pandya <74529230+LotiP26@users.noreply.github.com>
2022-10-01 12:21:52 -0700

    First!

---
+++ src/Hello.java
@@ -0,0 +1,21 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int number = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int result = mySecondNumber + myThirdNumber;
+        int myLastOne = result - 1000;
+        System.out.println(myLastOne);
+        System.out.println(result);
+        System.out.println(number);
+
+        short newNumber = Short.MIN_VALUE;
+        short newNumber1 = Short.MAX_VALUE;
+        System.out.println(newNumber);
+        System.out.println(newNumber1);
+
+
+    }
+}
